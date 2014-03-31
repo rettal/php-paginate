45043ace92a8d120c4130ba1eddc2c07eb5816de
rettal <dan.latter@gmail.com>
2014-03-31 18:45:12 +0100

    Adding example

---
+++ example.php
@@ -0,0 +1,38 @@
+<?php
+ini_set('display_errors', 1);
+
+require_once('vendor/autoload.php');
+
+// config + model
+$config = array(
+    'classes' => array('clearfix', 'pagination'),
+    'numPagingLinksToShow' => 10,
+    'recordsPerPage' => 20,
+    'targetPath' => '',
+    'nextLinkTxt' => 'Next &raquo;',
+    'prevLinkTxt' => '&laquo; Previous',
+    'currentPage' => isset($_GET['page']) ? ((int) $_GET['page']) : 1,
+    'itemsTotal' => 500,
+    'key' => 'page'
+);
+$pagerClassName = 'Pagination';
+$pagerConfig = array('name' => $pagerClassName, 'config' => $config);
+
+// view 1
+$pagerViewClassName = 'Numbered';
+$pagerViewConfig = array('name' => $pagerViewClassName, 'options' => array('showNextAndPrevLinks' => true));
+
+// view 2
+//$pagerViewClassName = 'NextAndPrev';
+//$pagerViewConfig = array('name' => $pagerViewClassName);
+
+// init
+$ps = new \Paginate\PaginationService();
+try {
+    $ps->initPagination($pagerViewConfig, $pagerConfig);
+}catch(Exception $e) {
+    echo $e->getMessage();
+}
+
+// output; in this case html
+echo $ps->getPaginationView()->getOutput();
